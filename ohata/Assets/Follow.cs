1b1ead156f05acdb440c49ddebf41300e70c59e8
Thiago <troyom51@gmail.com>
2021-04-01 19:55:40 -0300

    Tarefa

---
+++ ohata/Assets/Follow.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    //Ojetivo
+    Transform goal;
+
+    //Velocidade
+    public float speed = 5.0f;
+    
+    //Precisão
+    public float accuracy = 1.0f;
+   
+    //Rotação
+    public float rotSpeed = 2.0f;
+
+    // Indentificar o wpmanager
+    public GameObject wpManager;
+    
+    //Index
+    GameObject[] wps;
+    
+    //Nos
+    GameObject currentNode;
+    
+    //Origem
+    int currentWP = 0;
+    
+    //Graph
+    Graph g;
+
+    void Start()
+    {
+        //Chama componentes
+        wps = wpManager.GetComponent<WPManager>().waypoints;
+        g = wpManager.GetComponent<WPManager>().graph;
+        currentNode = wps[0];
+    }
+
+    //Leva parao heliporto
+    public void GoToHeli()
+    {
+        g.AStar(currentNode, wps[1]);
+        currentWP = 0;
+    }
+
+    //Leva para a Ruina
+    public void GoToRuin()
+    {
+        g.AStar(currentNode, wps[6]);
+        currentWP = 0;
+    }
+
+    void LateUpdate()
+    {
+        //Caminhada
+        if (g.getPathLength() == 0 || currentWP == g.getPathLength())
+            return;
+
+        //Muda para o ponto mais proximo
+        currentNode = g.getPathPoint(currentWP);
+
+        //Muda para o próximo 
+        if (Vector3.Distance(g.getPathPoint(currentWP).transform.position, transform.position) < accuracy)
+        {
+            currentWP++;
+        }
+
+        // faz a movimentação para outro sentido, mirando em outro ponto
+        if (currentWP < g.getPathLength())
+        {
+            goal = g.getPathPoint(currentWP).transform;
+            Vector3 lookAtGoal = new Vector3(goal.position.x, this.transform.position.y, goal.position.z);
+            Vector3 direction = lookAtGoal - this.transform.position;
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * rotSpeed);
+        }
+
+        transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+}
---
+++ ohata/Assets/WPManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// transporta dados para diferentes tipos de classe
+[System.Serializable]
+
+public struct Link
+{
+    //Ida e volta = BI, Uni = ida
+    public enum direction {UNI, BI}
+    //ligação de ponto a ponto
+    public GameObject node1;
+    public GameObject node2;
+    //Direção
+    public direction dir;
+}
+
+public class WPManager : MonoBehaviour
+{
+    // deixa publico um lugar para colocar oq voce define como waypoint
+    // public Transform[] waypoints;
+
+    //Array de waypoints
+    public GameObject[] waypoints;
+    // Array de informações
+    public Link[] links;
+    // Arquivos da pasta Graph
+    public Graph graph = new Graph();
+
+    void Start()
+    {
+        // se a quantidade de waypoint é maior que 0 ...
+        if(waypoints.Length > 0)
+        {
+            //É um loop do Array de waypoints
+            foreach(GameObject wp in waypoints)
+            {
+                // reabastece seu waypoint
+                graph.AddNode(wp);
+            }
+            //liga os waypoint
+            foreach(Link l in links)
+            {
+                graph.AddEdge(l.node1, l.node2);
+                // se ele for  BI ele faz o caminho inverso
+                if (l.dir == Link.direction.BI)
+                    graph.AddEdge(l.node2, l.node1);
+            }
+        }
+    }
+
+    void Update()
+    {
+        //Traçar a linha
+        graph.debugDraw(); 
+    }
+}
